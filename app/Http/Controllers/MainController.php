7a8491476546bde75fbc1d6eef35a2aa48990c67
Andrey Arzamaskov <aarzamaskov@simtechdev.org>
2021-08-17 22:49:07 +0400

    Create new action in MainController: parse

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,22 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use Exception;
 use Illuminate\Http\Request;
 
 /**
- * MainController
- *
+ * MainController.
  */
 class MainController extends Controller
 {
     /**
-     * index
+     * index.
      *
      * @return void
      */
     public function index()
     {
         return view('index');
+    }
+
+    public function parse(Request $request)
+    {
+        $param = [
+            'url' => $request['url'],
+        ];
+        return view('index', $param);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
 
 Route::get('/', [ MainController::class, 'index' ]);
+
+Route::get('/parse', [ MainController::class, 'parse' ]);
